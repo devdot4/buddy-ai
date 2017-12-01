8fcfc48a02dbf4369486fc12d37c96da62a2a8e2
Dev <dev.4@protonmail.com>
2017-11-30 20:19:31 -0500

    add simple chat function and comments

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,19 @@
-$(document).ready(function () {
-    // switch question with input on click
-    $('#btn1, #btn2, #btn3').click(function() {
-      $('#chat, #mood').toggle();
-    });
-    $('#buddy').hover(function () {
-        $(this).attr('src', 'images/1f604.png');
-    },
-    function () {
-        $(this).attr('src', 'images/1f600.png');
-    }
-    );
+$(document).ready(function() {
+  // switch question with input on click
+  $('#btn1, #btn2, #btn3').click(function() {
+    $('#chat, #mood').toggle();
+  });
+  // change emoji on hover
+  $('#buddy').hover(function() {
+    $(this).attr('src', 'images/1f604.png');
+  }, function() {
+    $(this).attr('src', 'images/1f600.png');
+  });
+  // user post
+  $('#add').on('click', function() {
+    var user = 'you'
+    var text = $('#idea').val();
+    var li = '<br/>' + '<li>' + user + '<br/>' + text + '</li>';
+    $('#list').append(li);
+  });
 });
