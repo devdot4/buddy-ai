c5dcfd3cfa8848b8afefdc3e0e7015041129d058
Dev <dev.4@protonmail.com>
2017-11-30 19:52:05 -0500

    add twitter emojis, md-preview and hover on title

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,13 @@
-// switch question with input on click
-$('#btn1, #btn2, #btn3').click(function() {
-  $('#chat, #mood').toggle();
+$(document).ready(function () {
+    // switch question with input on click
+    $('#btn1, #btn2, #btn3').click(function() {
+      $('#chat, #mood').toggle();
+    });
+    $('#buddy').hover(function () {
+        $(this).attr('src', 'images/1f604.png');
+    },
+    function () {
+        $(this).attr('src', 'images/1f600.png');
+    }
+    );
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'bud;Dy'});
+  res.render('index', {title: 'bud:Dy'});
 });
 
 module.exports = router;
